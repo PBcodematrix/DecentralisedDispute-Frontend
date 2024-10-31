c3989d298040fb6afb15944c9db923095b43878d
PBcodematrix <piyushboorra@gmail.com>
2024-10-31 15:01:24 +0530

    initial commit

---
+++ src/utils/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// const BASE_URL="http://localhost:5000"
+const BASE_URL="https://disputebackend.vercel.app"
+
+
+const axiosInstance = axios.create({ baseURL: BASE_URL });
+axios.interceptors.request.use(request => {
+  console.log('Starting Request', request)
+  return request; 
+})
+axios.interceptors.response.use(
+  (response) => response,
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || "Something went wrong"
+    )
+);
+
+export default axiosInstance;
